28c79f1ac144f928c56bc50c647598bdf27591b8
Andreas Krienke <shapoc@gmail.com>
2015-03-30 00:52:18 +0200

    ListView Divider

--- app/src/main/java/de/andreaskrienke/android/droidcavation/MainActivity.java
+++ app/src/main/java/de/andreaskrienke/android/droidcavation/MainActivity.java
@@ -6,8 +6,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
@@ -16,6 +14,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setButtonStates();
 
         initListeners();
     }
@@ -42,25 +42,46 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Set initial state of start buttons.
+     */
+    private void setButtonStates() {
+
+        findViewById(R.id.btn_start_sunits).setEnabled(true);
+
+        findViewById(R.id.btn_start_sites).setEnabled(false);
+        findViewById(R.id.btn_start_projects).setEnabled(false);
+        findViewById(R.id.btn_start_features).setEnabled(false);
+        findViewById(R.id.btn_start_squares).setEnabled(false);
+        findViewById(R.id.btn_start_media).setEnabled(false);
+    }
+
+    /**
+     * Initializes listeners for start buttons
+     */
     private void initListeners() {
         //our activity is click listener for btn_start_sunits
         findViewById(R.id.btn_start_sunits).setOnClickListener(this);
+
         //our activity is click listener for btn_start_features
-        findViewById(R.id.btn_start_features).setOnClickListener(this);
+        //findViewById(R.id.btn_start_features).setOnClickListener(this);
+
         //our activity is click listener for btn_start_projects
-        findViewById(R.id.btn_start_projects).setOnClickListener(this);
+        //findViewById(R.id.btn_start_projects).setOnClickListener(this);
+
         //our activity is click listener for btn_start_sites
-        findViewById(R.id.btn_start_sites).setOnClickListener(this);
+        //findViewById(R.id.btn_start_sites).setOnClickListener(this);
+
         //our activity is click listener for btn_start_squares
-        findViewById(R.id.btn_start_squares).setOnClickListener(this);
+        //findViewById(R.id.btn_start_squares).setOnClickListener(this);
+
         //our activity is click listener for btn_start_media
-        findViewById(R.id.btn_start_media).setOnClickListener(this);
+        //findViewById(R.id.btn_start_media).setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, ((Button) v).getText(), Toast.LENGTH_SHORT).show();
 
         if (v.getId() == R.id.btn_start_sunits) {
             Intent intent = new Intent(this, SUnitListActivity.class);
